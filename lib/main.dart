3dd484c0a8639aefe74b258e0a9f24117a9f085e
ImagineCode <imaginecode.rv@gmail.com>
2026-05-11 16:29:15 -0300

    feat: integração com Firebase Auth e Firestore no login e cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'login_page.dart';
 import 'theme/app_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
