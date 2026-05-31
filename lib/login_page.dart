54345d3443135fffc3fd21fc081ff03e35a9c646
y1990y <edurisso05@gmail.com>
2026-05-31 15:43:11 -0300

    Correção import

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
-import 'register_page.dart';
+import 'pages/register_page.dart';
 import 'theme/app_fonts.dart';
 
 class LoginPage extends StatefulWidget {
